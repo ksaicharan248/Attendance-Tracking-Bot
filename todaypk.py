2ef741fad7e2335cfad8c7b657b9c52ae90958c9
Sai charan <saicharanreddy11102003@gmail.com>
2023-03-20 21:47:44 +0530

    added new features

--- todaypk.py
+++ todaypk.py
@@ -2,18 +2,19 @@ from selenium import webdriver
 from selenium.common import WebDriverException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from pytz import timezone
-from datetime import datetime
 import time
+from PIL import Image
+import io
+import base64
 
 
-def today():
+def today(ind_time):
     try:
-        ind_time = datetime.now(timezone("Asia/Kolkata")).strftime('%d/%m/%Y')
         opt = Options()
         opt.add_argument('--headless')
         opt.add_argument('--no-sandbox')
         driver = webdriver.Chrome(options=opt)
+        driver.set_window_size(1024, 768)
         driver.get("http://bit.ly/3Qb3MoX")
         driver.get("http://bit.ly/3Qb3MoX")
         driver.get("http://117.239.51.140/sitams/Academics/StudentAttendance.aspx?showtype=SA")
@@ -21,18 +22,36 @@ def today():
         driver.find_element(By.CSS_SELECTOR, '#txtFromDate').send_keys(ind_time)
         driver.find_element(By.CSS_SELECTOR, '#txtToDate').send_keys(ind_time)
         driver.find_element(By.CSS_SELECTOR, '#btnShow').click()
-        tm = [driver.find_element(By.CSS_SELECTOR,
-                                  f"#tblReport > table > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child({i}) > td:nth-child(5)").text
-              for i in range(2, 15)]
-        total = driver.find_element(By.CSS_SELECTOR,
-                                    "#tblReport > table > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(15) > td:nth-child(4)").text
-        tm.append(total)
+        screenshot = driver.get_screenshot_as_png()
+        image = Image.open(io.BytesIO(screenshot)).convert('RGB')
+        cropped_image = image.crop((10, 160, 970, 700))
+        with io.BytesIO() as output:
+            cropped_image.save(output, format='JPEG')
+            image_bytes = output.getvalue()
+        encoded_string = base64.b64encode(image_bytes).decode('utf-8')
         driver.close()
-        return tm
+        return encoded_string
 
 
     except WebDriverException:
         return 'server not responding 404'
+
+
+def dato(s=""):
+    if "-" in s:
+        s = s.replace("-", "/")
+    ob = time.gmtime(time.time())
+    if s == "":
+        m = str(ob[2]) + '/' + str(ob[1]) + '/' + str(ob[0])
+    elif len(s) <= 2 and '/' not in s:
+        m = s + '/' + str(ob[1]) + '/' + str(ob[0])
+
+    elif '/' in s and len(s) <= 5:
+        m = s + '/' + str(ob[0])
+
+    else:
+        m = s
+    return m
 
 
 if __name__ == "__main__":
