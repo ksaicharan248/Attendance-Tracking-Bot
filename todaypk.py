a6b67becef42237bf92d52d8beb0717a717a6ad2
ksaicharan248 <83259388+ksaicharan248@users.noreply.github.com>
2023-03-24 07:16:44 +0530

    Update todaypk.py

--- todaypk.py
+++ todaypk.py
@@ -1,94 +1,69 @@
-import time
-import asyncio
-from pyppeteer import launch
-from aiogram import *
+from selenium import webdriver
+from selenium.common import WebDriverException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 import time
 from PIL import Image
 import io
 import base64
 
 
-#
-
-async def goget(x):
+def today(ind_time):
     try:
-        y = str(x)
-        browser = await launch(executablePath='/usr/bin/google-chrome-stable', headless=True, args=['--no-sandbox'])
-        page = await browser.newPage()
-        await page.goto('http://bit.ly/3Qb3MoX')
-        await page.goto('http://bit.ly/3Qb3MoX')
-        await page.goto("http://117.239.51.140/sitams/Academics/StudentAttendance.aspx?")
-        if len(y) == 2:
-            await page.type('#txtRollNo', '20751A04' + y)
-        elif 'l' in y:
-            n = y.replace("l", "")
-            await page.type('#txtRollNo', '21755A04' + n)
-        elif 'l' not in y and len(y) == 3:
-            await page.type('#txtRollNo', '20751A0' + y)
-        await page.click('#radTillNow')
-        await page.click('#btnShow')
-        screenshot = await page.screenshot(fullPage=True)
+        opt = Options()
+        opt.add_argument('--headless')
+        opt.add_argument('--no-sandbox')
+        driver = webdriver.Chrome(options=opt)
+        driver.set_window_size(1024, 768)
+        driver.get("http://bit.ly/3Qb3MoX")
+        driver.get("http://bit.ly/3Qb3MoX")
+        driver.get("http://117.239.51.140/sitams/Academics/StudentAttendance.aspx?showtype=SA")
+        driver.find_element(By.CSS_SELECTOR, '#radPeriod').click()
+        driver.find_element(By.CSS_SELECTOR, '#txtFromDate').send_keys(ind_time)
+        driver.find_element(By.CSS_SELECTOR, '#txtToDate').send_keys(ind_time)
+        driver.find_element(By.CSS_SELECTOR, '#btnShow').click()
+        screenshot = driver.get_screenshot_as_png()
         image = Image.open(io.BytesIO(screenshot)).convert('RGB')
-        cropped_image = image.crop((10, 160, 760, 700))
+        cropped_image = image.crop((10, 160, 970, 700))
         with io.BytesIO() as output:
             cropped_image.save(output, format='JPEG')
             image_bytes = output.getvalue()
         encoded_string = base64.b64encode(image_bytes).decode('utf-8')
-        await browser.close()
+        driver.close()
         return encoded_string
 
-    except:
-        return "Nothing found"
+
+    except WebDriverException:
+        return 'server not responding 404'
 
 
-async def date_attendance_67(ind_time):
+def today_rs(ind_time):
     try:
-        browser = await launch(executablePath='/usr/bin/google-chrome-stable', headless=True, args=['--no-sandbox'])
-        page = await browser.newPage()
-        await page.goto('http://bit.ly/3Qb3MoX')
-        await page.goto('http://bit.ly/3Qb3MoX')
-        await page.goto('http://117.239.51.140/sitams/Academics/StudentAttendance.aspx?showtype=SA')
-        await page.click('#radPeriod')
-        await page.type('#txtFromDate', ind_time)
-        await page.type('#txtToDate', ind_time)
-        await page.click('#btnShow')
-        screenshot = await page.screenshot(fullPage=True)
+        opt = Options()
+        opt.add_argument('--headless')
+        opt.add_argument('--no-sandbox')
+        driver = webdriver.Chrome(options=opt)
+        driver.set_window_size(1024, 768)
+        driver.get("http://bit.ly/3WUQsHy")
+        driver.get("http://bit.ly/3WUQsHy")
+        driver.get("http://117.239.51.140/sitams/Academics/StudentAttendance.aspx?showtype=SA")
+        driver.find_element(By.CSS_SELECTOR, '#radPeriod').click()
+        driver.find_element(By.CSS_SELECTOR, '#txtFromDate').send_keys(ind_time)
+        driver.find_element(By.CSS_SELECTOR, '#txtToDate').send_keys(ind_time)
+        driver.find_element(By.CSS_SELECTOR, '#btnShow').click()
+        screenshot = driver.get_screenshot_as_png()
         image = Image.open(io.BytesIO(screenshot)).convert('RGB')
-        cropped_image = image.crop((10, 160, 760, 700))
+        cropped_image = image.crop((10, 160, 970, 700))
         with io.BytesIO() as output:
             cropped_image.save(output, format='JPEG')
             image_bytes = output.getvalue()
         encoded_string = base64.b64encode(image_bytes).decode('utf-8')
-        await browser.close()
+        driver.close()
         return encoded_string
 
-    except:
-        return "Nothing found"
 
-
-async def date_attendance_32(ind_time):
-    try:
-        browser = await launch(executablePath='/usr/bin/google-chrome-stable', headless=True, args=['--no-sandbox'])
-        page = await browser.newPage()
-        await page.goto('http://bit.ly/3WUQsHy')
-        await page.goto('http://bit.ly/3WUQsHy')
-        await page.goto('http://117.239.51.140/sitams/Academics/StudentAttendance.aspx?showtype=SA')
-        await page.click('#radPeriod')
-        await page.type('#txtFromDate', ind_time)
-        await page.type('#txtToDate', ind_time)
-        await page.click('#btnShow')
-        screenshot = await page.screenshot(fullPage=True)
-        image = Image.open(io.BytesIO(screenshot)).convert('RGB')
-        cropped_image = image.crop((10, 160, 760, 700))
-        with io.BytesIO() as output:
-            cropped_image.save(output, format='JPEG')
-            image_bytes = output.getvalue()
-        encoded_string = base64.b64encode(image_bytes).decode('utf-8')
-        await browser.close()
-        return encoded_string
-
-    except:
-        return "nothing found"
+    except WebDriverException:
+        return 'server not responding 404'
 
 
 def dato(s=""):
@@ -108,18 +83,9 @@ def dato(s=""):
     return m
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     start_time = time.time()
-
-
-    async def no():
-        encoded_string = await goget(232)
-        decoded_bytes = base64.b64decode(str(encoded_string))
-        photo_file = io.BytesIO(decoded_bytes)
-        boto = Bot(token='6259883364:AAHdH_wd1-uaxD4EUw6kgBRbcM3MX40WUEw')
-        await boto.send_photo(chat_id=1746861239, photo=photo_file)
-
-
-    asyncio.get_event_loop().run_until_complete(no())
+    t3 = today()
     end_time = time.time()
     print(f"Time taken: {end_time - start_time:.2f} seconds")
+    print(t3)
