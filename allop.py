4a88cc4da237b2edca3273693f55f6a9de95e027
EC2 Default User <ec2-user@ip-172-31-46-83.ap-south-1.compute.internal>
2023-05-21 14:35:37 +0000

    the unwanted spaces rremoved

--- allop.py
+++ allop.py
@@ -201,9 +201,8 @@ if __name__ == "__main__" :
             t2 = batchroll()
             await bot.send_message(chat_id=1746861239,text="sno " + " " * (6 - len(str("s.no"))) + "roll num " + " " * (5 - len("roll num")) + " " + " " * (7 - len(str("percentage"))) + "percentage " + "\n" + "\n".join([str(i + 1) + " " * (9 - len(str(i + 1))) + str(t2[0][i]) + " " * (8 - len(str(t2[0][i]))) + ":" + " " * (10 - len(str(t2[1][i]))) + str(t2[1][i]) + " %" for i in range(0, len(t2[0]))]))
 
-  
 
-   
+
     asyncio.run(getoo())
     end_time = time.time()
     print("done")
