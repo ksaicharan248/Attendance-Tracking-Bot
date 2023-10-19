f0625a8c61a820333774fe7bcf6c48d1eb9e4a8c
Sai charan <saicharanreddy11102003@gmail.com>
2023-10-19 19:37:44 +0530

    update

--- allop.py
+++ allop.py
@@ -33,7 +33,7 @@ def goget(x) :
         driver.find_element(By.CSS_SELECTOR , '#radTillNow').click()
         driver.find_element(By.CSS_SELECTOR , '#btnShow').click()
         div_element = driver.find_element('css selector' , '#tblReport')
-        size_of_frame = int(div_element.get_window_size().get('height')) + 10
+        size_of_frame = int(div_element.size['height']) +10
         driver.execute_script(f'document.querySelector("#tblReport").style.height ="{size_of_frame}px";')
         screenshot = div_element.screenshot_as_png
         image = Image.open(io.BytesIO(screenshot)).convert('RGB')
